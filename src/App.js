d0d22072697b84d24cec824cc27c3bd4b325a33e
Thomas Pendleton <thomas6576@yahoo.com>
2022-04-03 21:45:08 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import Home from './routes/home/home.componenet.jsx'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   )
 }
