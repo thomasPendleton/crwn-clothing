52500932464659e1b7066991a1034cd0e86088b2
Thomas Pendleton <thomas6576@yahoo.com>
2022-04-04 21:15:26 -0700

    adding routes and navigation, added firebase auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './routes/home/home.componenet.jsx'
+import Navigation from './routes/navigation/navigation.component.jsx'
+import SignIn from './routes/sign-in/sign-in.component.jsx'
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='/shop' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,47 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth'
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAPPAWNrRDmGWIX2CAoSXgPLQDHA5r2AOI',
+  authDomain: 'crwn-clothing-14b94.firebaseapp.com',
+  projectId: 'crwn-clothing-14b94',
+  storageBucket: 'crwn-clothing-14b94.appspot.com',
+  messagingSenderId: '136527317810',
+  appId: '1:136527317810:web:eb0b012dbd957bf7caa52b',
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+  prompt: 'select_account',
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users' , userAuth.uid)
+    console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef)
+    
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+
+        } catch {
+
+        }
+    }
+}
